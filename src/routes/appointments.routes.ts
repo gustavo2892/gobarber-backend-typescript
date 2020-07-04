7a6c76605c494ebfae0e339ea3bb6f6f18effe37
gustavo2892 <gustavo2892@gmail.com>
2020-07-04 11:54:50 -0300

    List all repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,14 +4,20 @@ import { startOfHour, parseISO } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentRepository();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointmentRepository.findByDate(
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
     parsedDate,
   );
 
@@ -21,7 +27,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
