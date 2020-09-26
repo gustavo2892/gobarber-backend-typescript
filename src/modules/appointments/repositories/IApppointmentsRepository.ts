4d3d0ecc4aa127476268121bc39a0bb850098591
gustavo2892 <gustavo2892@gmail.com>
2020-09-26 16:25:37 -0300

    Dependency Inversion Principle

--- src/modules/appointments/repositories/IApppointmentsRepository.ts
+++ src/modules/appointments/repositories/IApppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateApppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
