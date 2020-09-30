98c2ec2798eec6cf2d1b6af123076e7bc89909ff
gustavo2892 <gustavo2892@gmail.com>
2020-09-30 14:41:49 -0300

    Create User Test

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
